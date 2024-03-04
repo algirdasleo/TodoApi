3396bb7930e63d9a43263dfceb946aef3c837b96
Plexukas <algirdasleonardas@gmail.com>
2024-03-04 16:00:29 +0200

    Pasalintas [required] atributas prie ID

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -3,7 +3,6 @@ namespace TodoApi.Models
 {
     public class TodoItem
     {
-        [Required] // nurodo, kad Id negali buti null
         public long Id { get; set; } // Id laukas, kuris bus naudojamas kaip primary key
         [Required] 
         public string Name { get; set; } = string.Empty; // Name laukas, kuris bus naudojamas kaip uzduoties pavadinimas
