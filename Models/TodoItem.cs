3c3aaed3be663f070c6e2b3b4697d0f19455e743
Plexukas <algirdasleonardas@gmail.com>
2024-03-07 19:38:18 +0200

    comment changes

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -3,9 +3,9 @@ namespace TodoApi.Models
 {
     public class TodoItem
     {
-        public long Id { get; set; } // Id laukas, kuris bus naudojamas kaip primary key
+        public long Id { get; set; } // Id laukas, kuris naudojamas kaip primary key
         [Required] 
-        public string Name { get; set; } = string.Empty; // Name laukas, kuris bus naudojamas kaip uzduoties pavadinimas
+        public string Name { get; set; } = string.Empty; // Name laukas, kuris naudojamas kaip uzduoties pavadinimas
         public bool IsComplete { get; set; } // Bool, kuris rodo ar uzduotis yra atlikta
     }
 }
